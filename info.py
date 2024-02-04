54bc744dd188f6c01b61295bd3d1317a0c0159b6
sahasudip2006 <127957723+sahasudip2006@users.noreply.github.com>
2024-02-04 12:10:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,6 +80,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1001565949579'))
 
 LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
 
